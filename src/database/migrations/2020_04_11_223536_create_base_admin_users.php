1756ee3c598f56418a2140d415ee5a9957fe680f
Andres Bastidas <andres@bastidas.us>
2020-04-17 20:31:02 -0400

    Created migrations to create the administrator user Added a seed to fill the database with all the roles and permissions

--- src/database/migrations/2020_04_11_223536_create_base_admin_users.php
+++ src/database/migrations/2020_04_11_223536_create_base_admin_users.php
@@ -18,14 +18,14 @@ class CreateBaseAdminUsers extends Migration
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
         // gets all permissions via Gate::before rule; see AuthServiceProvider
-        $role = Role::create(['name' => 'super-admin']);
+        $role = Role::create(['name' => 'admin']);
 
         // create admin user
         $user = Factory(User::class)->create([
             'email' => 'admin@app.com',
         ]);
 
-        // assign super-admin role
+        // assign admin role
         $user->assignRole($role);
     }
 
